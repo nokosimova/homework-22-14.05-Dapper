6c1c081448c5552f0ca9209e4a1b1a6014896318
kosimovanodira <nkosimova@gmail.com>
2020-05-18 17:17:56 +0500

    add CRUD methods

--- hw22-Dapper/Repositories/StudentRepository.cs
+++ hw22-Dapper/Repositories/StudentRepository.cs
@@ -30,6 +30,59 @@ namespace hw22_Dapper.Repositories
             }
         }
 
-
+        public int? Create(StudentList student)
+        {
+            try
+            {
+                using (IDbConnection con = new SqlConnection(ConnectionString))
+                {
+                    string newCommand = $"insert into StudentListVALUES('{student.StudentId}',{student.FirstName}" +
+                        $"{student.LastName}, {student.MidName}, {student.BirthDate}, {student.Telephone})";
+                    return con.Query<int>(newCommand).FirstOrDefault();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return null;
+            }
+        }
+        public int? Update(StudentList student, int Id)
+        {
+            try
+            {
+                using (IDbConnection con = new SqlConnection(ConnectionString))
+                {
+                    string newCommand = $"update StudentList set FisrtName = {student.FirstName}," +                                                              
+                                                               $"LastName = {student.LastName}, " +
+                                                               $"MidName = {student.MidName}, " +
+                                                               $"BirthDate = {student.BirthDate}," +
+                                                               $"Telephone = {student.Telephone} " +
+                                                               $"where StudentId = {student.StudentId}";
+                    return con.Query<int>(newCommand).FirstOrDefault();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error found: {ex.Message}");
+                return null;
+            }
+        }
+        public int? Delete(int Id)
+        {
+            try
+            {
+                using (IDbConnection con = new SqlConnection(ConnectionString))
+                {
+                    string newCommand = $"delete from StudentList where StudentId = {Id}";
+                    return con.Query<int>(newCommand).FirstOrDefault();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error found: {ex.Message}");
+                return null;
+            }
+        }
     }
 }
