562bbdb87e2bd459b59991629d63c1423a22d998
kosimovanodira <nkosimova@gmail.com>
2020-05-18 17:02:43 +0500

    add Read method

--- hw22-Dapper/Repositories/Repository.cs
+++ hw22-Dapper/Repositories/Repository.cs
@@ -1,19 +1,26 @@
-﻿using System;
+﻿
+using System;
+
 using System.Collections.Generic;
+
+using System.Data;
+
 using System.Text;
 
+using System.Data.SqlClient;
+
+using System.Linq;
+
+using Dapper;
 
 namespace hw22_Dapper.Repositories
 {
-    class Repository
-    {
-        public abstract class RepositoryBase<TModel>
+    public abstract class RepositoryBase<TModel>
         {
             public string ConnectionString { get; set; }
             public RepositoryBase()
             {
-                ConnectionString = "Data source=KosimovaNodira; Initial catalog = NewDB; Integrated Security=True";
+                ConnectionString = "Data source=KosimovaNodira; Initial catalog = NewDB; Integrated Security=True;";
             }
-        }
     }
 }
--- hw22-Dapper/Repositories/StudentRepository.cs
+++ hw22-Dapper/Repositories/StudentRepository.cs
@@ -1,10 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
+using Dapper;
+using System.Data;
+using hw22_Dapper.Models;
+using System.Data.SqlClient;
+using System.Linq;
 using System.Text;
+using System.Collections.Generic;
 
 namespace hw22_Dapper.Repositories
 {
-    class StudentRepository
+    class StudentRepository : RepositoryBase<StudentList>
     {
+        public StudentRepository():base()
+        {}
+
+        public List<StudentList>Read()
+        {
+            try
+            {
+                using (IDbConnection con = new SqlConnection(ConnectionString))
+                {
+                    return con.Query<StudentList>($"select * from {typeof(StudentList).Name?.ToUpper()}").ToList();
+                }
+            }
+            catch (SqlException x)
+            {
+                Console.WriteLine($"Error:{x.Message}");
+                return null;
+            }
+        }
+
+
     }
 }
